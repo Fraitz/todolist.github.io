c5f22098a9f0bd982a7734235bd65861487723ba
Matheus Duarte de Freitas <freickup@gmail.com>
2021-06-03 07:26:26 -0300

    correcao dos arquivos

--- script.js
+++ script.js
@@ -1,305 +1,118 @@
-@import url('https://fonts.googleapis.com/css2?family=Ubuntu&display=swap');
+const listaOrdenada = document.querySelector('#lista-tarefas');
+const botaoCriarTask = document.getElementById('criar-tarefa');
+const inputField = document.querySelector('#texto-tarefa');
+const botaoRemoveTudo = document.querySelector('#apaga-tudo');
+const botaoRemoveFinalizados = document.querySelector('#remover-finalizados');
+const botaoSalvarTarefas = document.querySelector('#salvar-tarefas');
+const botaoRemoveSeleccionado = document.querySelector('#remover-selecionado');
+const botaoMoveBaixo = document.querySelector('#mover-baixo');
+const botaoMoveCima = document.querySelector('#mover-cima');
 
-@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@1,300&family=Ubuntu&display=swap');
-
-@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@1,300&family=Roboto&family=Ubuntu&display=swap');
-
-/* Id's */
-body {
-  background-color: rgb(236 , 236 , 236);
+function criaTarefa() {
+  const inputTask = inputField.value;
+  if (inputTask === '') {
+    return null;
+  }
+  const elementoDaLista = document.createElement('li');
+  elementoDaLista.innerHTML = inputTask;
+  elementoDaLista.className = 'tarefa';
+  listaOrdenada.appendChild(elementoDaLista);
+  inputField.value = '';
 }
 
-header {
-  margin-left: 37%;
+botaoCriarTask.addEventListener('click', criaTarefa);
+
+// Matheus Camillo me ajudou a reduzir este código, por capricho, atribuíndo estilo na classe selected no CSS e não aqui no JS.
+function coloreItem(origem) {
+  const itemSelecionado = document.querySelector('.selected');
+  if (origem.target.className === 'tarefa') {
+    origem.target.classList.add('selected');
+    if (itemSelecionado !== null) {
+      itemSelecionado.classList.remove('selected');
+    }
+  }
 }
 
-main {
-  margin-left: 35%;
+listaOrdenada.addEventListener('click', coloreItem);
+
+function riscaItem(origem) {
+  if (origem.target.classList.contains('completed') === false) {
+    origem.target.classList.add('completed');
+  } else if (origem.target.classList.contains('completed')) {
+    origem.target.classList.remove('completed');
+  }
 }
 
-ol {
-  display: inline-block;
-  margin: 5px 0;
-  padding-left: 0;
-  list-style: none;
+listaOrdenada.addEventListener('dblclick', riscaItem);
+
+function apagaTudo() {
+  while (listaOrdenada.lastElementChild !== null) {
+    listaOrdenada.removeChild(listaOrdenada.lastElementChild);
+  }
 }
 
-button {
-  font-family: Roboto, sans-serif;
+botaoRemoveTudo.addEventListener('click', apagaTudo);
+
+function removeFinalizados() {
+  const finalizados = document.querySelectorAll('.completed');
+  for (let index = 0; index < finalizados.length; index += 1) {
+    if (finalizados[index].classList.contains('completed')) {
+      listaOrdenada.removeChild(finalizados[index]);
+    }
+  }
 }
 
-p {
-  font-family: Lato, sans-serif;
-  font-weight: 800;
-  font-size: larger;
-  margin-top: 5px;
+botaoRemoveFinalizados.addEventListener('click', removeFinalizados);
+
+function salvarTarefas() {
+  const htmlDaLista = listaOrdenada.innerHTML;
+  localStorage.setItem('listaSalva', htmlDaLista);
 }
 
-#titulo {
-  font-family: Ubuntu, sans-serif;
-  margin-bottom: 0;
+function recuperaTarefas() {
+  const listaSalva = localStorage.getItem('listaSalva');
+  if (listaSalva !== null) {
+    listaOrdenada.innerHTML = listaSalva;
+  }
 }
 
-#sidenav {
-  height: 100%;
-  width: 30%;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  background-color: rgb(78 , 129 , 121);
-  overflow-x: hidden;
-  box-shadow: 0 0 5px 5px rgba(114 , 114 , 114 , 0.404);
+recuperaTarefas();
+
+// Para criar este evento corretamente utilizei estas literaturas: https://stackabuse.com/storing-data-in-the-browser-with-localstorage , https://blog.logrocket.com/localstorage-javascript-complete-guide/ , https://projects.raspberrypi.org/en/projects/cd-intermediate-javascript-sushi/7 (esta última sem a necessidade das funções com JSON pois usei a string de HTML diretamente aplicada à Inner html), https://tableless.com.br/guia-f%C3%A1cil-sobre-usar-localstorage-com-javascript/, https://www.w3schools.com/jsref/met_storage_setitem.asp, https://www.w3schools.com/jsref/met_storage_getitem.asp
+
+botaoSalvarTarefas.addEventListener('click', salvarTarefas);
+
+function removeSelecionado() {
+  const selecionado = document.querySelector('.selected');
+  if (selecionado !== null) {
+    listaOrdenada.removeChild(selecionado);
+  }
 }
 
-#topSideMenu {
-  background: white;
-  width: 100%;
-  top: 0;
-  padding: 20px 30px 10px;
-  margin-bottom: 40px;
+botaoRemoveSeleccionado.addEventListener('click', removeSelecionado);
+
+function moveParaBaixo() {
+  const selecionado = document.querySelector('.selected');
+  if (selecionado !== null) {
+    const elementoDeBaixo = selecionado.nextElementSibling;
+    if (selecionado !== listaOrdenada.lastChild) {
+      listaOrdenada.insertBefore(selecionado, elementoDeBaixo.nextElementSibling);
+    }
+  }
 }
 
-#titleSideMenu {
-  margin: 0;
-  font-family: Ubuntu, sans-serif;
+// Para acionar este evento corretamente, utilizei estas literaturas https://developer.mozilla.org/pt-BR/docs/Web/API/Node/insertBefore e https://www.w3schools.com/jsref/prop_element_previouselementsibling.asp
+
+botaoMoveBaixo.addEventListener('click', moveParaBaixo);
+
+function moveParaCima() {
+  const selecionado = document.querySelector('.selected');
+  if (selecionado !== null) {
+    const elementoDeCima = selecionado.previousElementSibling;
+    if (selecionado !== listaOrdenada.firstChild) {
+      listaOrdenada.insertBefore(selecionado, elementoDeCima);
+    }
+  }
 }
 
-/* Menu do Input -- Início */
-#menuDoInput {
-  margin-left: 20px;
-}
-
-#texto-tarefa {
-  display: block;
-  border-radius: 50px;
-  margin-bottom: 15px;
-  width: 90%;
-  height: 40px;
-  border: 1px solid rgb(250 , 250 , 250);
-  box-shadow: 0 2px 4px 1px rgba(114 , 114 , 114 , 0.404);
-}
-
-#texto-tarefa:focus {
-  border-radius: 0;
-  background-color: -internal-light-dark white , rgb(187 , 187 , 187));
-  outline-color: white;
-  box-shadow: 0 2px 10px 1px rgba(114 , 114 , 114 , 0.76);
-}
-
-#texto-tarefa:hover {
-  box-shadow: 0 2px 10px 1px rgba(114 , 114 , 114 , 0.76);
-  cursor: pointer;
-}
-
-#criar-tarefa {
-  display: block;
-  border-radius: 50px;
-  width: 91%;
-  height: 45px;
-  border: 0;
-  background-color: rgb(80 , 21 , 136);
-  color: white;
-  box-shadow: 0 2px 4px 1px rgba(73 , 73 , 73 , 0.61);
-}
-
-#criar-tarefa:hover {
-  box-shadow: 0 2px 10px 1px rgba(49 , 49 , 49 , 0.76);
-  background-color: rgb(62 , 16 , 105);
-  cursor: pointer;
-  border-radius: 0;
-}
-
-#criar-tarefa:active {
-  background-color: rgb(80 , 21 , 136);
-  box-shadow: 0 0 0 0 rgba(151 , 151 , 151 , 0.76);
-  font-size: 13px;
-}
-
-/* Menu do Input -- Final */
-
-/* Menu dos Botões -- Início */
-#menuBotoes {
-  margin-left: 20px;
-  margin-top: 20px;
-}
-
-#apaga-tudo {
-  display: inline-block;
-  border-radius: 50px;
-  width: 43%;
-  height: 45px;
-  border: 0;
-  background-color: rgb(199 , 199 , 199);
-  color: black;
-  box-shadow: 0 2px 4px 1px rgba(73 , 73 , 73 , 0.61);
-}
-
-#apaga-tudo:hover {
-  box-shadow: 0 2px 10px 1px rgba(49 , 49 , 49 , 0.76);
-  background-color: rgb(180 , 180 , 180);
-  cursor: pointer;
-}
-
-#apaga-tudo:active {
-  background-color: rgb(199 , 199 , 199);
-  box-shadow: 0 0 0 0 rgba(151 , 151 , 151 , 0.76);
-  font-size: 13px;
-}
-
-#remover-finalizados {
-  display: inline-block;
-  border-radius: 50px;
-  width: 43%;
-  margin-left: 10px;
-  height: 45px;
-  border: 0;
-  background-color: rgb(199 , 199 , 199);
-  color: black;
-  box-shadow: 0 2px 4px 1px rgba(73 , 73 , 73 , 0.61);
-}
-
-#remover-finalizados:hover {
-  box-shadow: 0 2px 10px 1px rgba(49 , 49 , 49 , 0.76);
-  background-color: rgb(180 , 180 , 180);
-  cursor: pointer;
-}
-
-#remover-finalizados:active {
-  background-color: rgb(199 , 199 , 199);
-  box-shadow: 0 0 0 0 rgba(151 , 151 , 151 , 0.76);
-  font-size: 13px;
-}
-
-#menuBotoes2 {
-  margin-left: 20px;
-  margin-top: 20px;
-}
-
-#salvar-tarefas {
-  display: inline-block;
-  border-radius: 50px;
-  width: 43%;
-  height: 45px;
-  border: 0;
-  background-color: rgb(199 , 199 , 199);
-  color: black;
-  box-shadow: 0 2px 4px 1px rgba(73 , 73 , 73 , 0.61);
-}
-
-#salvar-tarefas:hover {
-  box-shadow: 0 2px 10px 1px rgba(49 , 49 , 49 , 0.76);
-  background-color: rgb(180 , 180 , 180);
-  cursor: pointer;
-}
-
-#salvar-tarefas:active {
-  background-color: rgb(199 , 199 , 199);
-  box-shadow: 0 0 0 0 rgba(151 , 151 , 151 , 0.76);
-  font-size: 13px;
-}
-
-#remover-selecionado {
-  display: inline-block;
-  border-radius: 50px;
-  width: 43%;
-  margin-left: 10px;
-  height: 45px;
-  border: 0;
-  background-color: rgb(199 , 199 , 199);
-  color: black;
-  box-shadow: 0 2px 4px 1px rgba(73 , 73 , 73 , 0.61);
-}
-
-#remover-selecionado:hover {
-  box-shadow: 0 2px 10px 1px rgba(49 , 49 , 49 , 0.76);
-  background-color: rgb(180 , 180 , 180);
-  cursor: pointer;
-}
-
-#remover-selecionado:active {
-  background-color: rgb(199 , 199 , 199);
-  box-shadow: 0 0 0 0 rgba(151 , 151 , 151 , 0.76);
-  font-size: 13px;
-}
-
-#menuBotoes3 {
-  margin-left: 20px;
-  margin-top: 20px;
-}
-
-#mover-cima {
-  display: inline-block;
-  border-radius: 50px;
-  width: 43%;
-  height: 45px;
-  border: 0;
-  background-color: rgb(35 , 105 , 211);
-  color: white;
-  box-shadow: 0 2px 4px 1px rgba(73 , 73 , 73 , 0.61);
-}
-
-#mover-cima:hover {
-  box-shadow: 0 2px 10px 1px rgba(49 , 49 , 49 , 0.76);
-  background-color: rgb(32 , 88 , 173);
-  cursor: pointer;
-  border-radius: 0;
-}
-
-#mover-cima:active {
-  background-color: rgb(35 , 105 , 211);
-  box-shadow: 0 0 0 0 rgba(151 , 151 , 151 , 0.76);
-  font-size: 13px;
-}
-
-#mover-baixo {
-  display: inline-block;
-  border-radius: 50px;
-  width: 43%;
-  margin-left: 10px;
-  height: 45px;
-  border: 0;
-  background-color: rgb(160 , 9 , 67);
-  color: white;
-  box-shadow: 0 2px 4px 1px rgba(73 , 73 , 73 , 0.61);
-}
-
-#mover-baixo:hover {
-  box-shadow: 0 2px 10px 1px rgba(49 , 49 , 49 , 0.76);
-  background-color: rgb(119 , 6 , 49);
-  cursor: pointer;
-  border-radius: 0;
-}
-
-#mover-baixo:active {
-  background-color: rgb(160 , 9 , 67);
-  box-shadow: 0 0 0 0 rgba(151 , 151 , 151 , 0.76);
-  font-size: 13px;
-}
-
-/* Classes */
-.tarefa {
-  font-family: Roboto, sans-serif;
-  padding: 15px;
-  margin: 12px 10px;
-  background-color: white;
-  max-width: 95%;
-  box-shadow: 0 2px 4px 1px rgba(73 , 73 , 73 , 0.61);
-  min-width: 55vw;
-}
-
-.selected {
-  background-color: grey;
-}
-
-.tarefa:hover {
-  cursor: pointer;
-}
-
-.completed {
-  text-decoration: line-through;
-}
-
-.fas {
-  margin-right: 7px;
-}
+botaoMoveCima.addEventListener('click', moveParaCima);
